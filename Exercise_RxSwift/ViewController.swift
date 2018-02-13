04d8711f8a713f0ff06a09e7226bbc6af7fb233e
He Wu <he.wu@linecorp.com>
2018-02-13 16:10:57 +0900

    Initial Commit

---
+++ Exercise_RxSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exercise_RxSwift
+//
+//  Created by ST21235 on 2018/02/13.
+//  Copyright © 2018 He Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
