527a97a7702e690dd6f9b084488eafa9928afe74
He Wu <he.wu@linecorp.com>
2018-02-14 18:32:17 +0900

    Implement Save Current Image function

--- Practice_RxSwift_Project/Practice_Observables/Combinestagram/PhotoWriter.swift
+++ Practice_RxSwift_Project/Practice_Observables/Combinestagram/PhotoWriter.swift
@@ -23,11 +23,36 @@
 import Foundation
 import UIKit
 import Photos
+import RxSwift
 
 class PhotoWriter {
     enum Errors: Error {
         case couldNotSavePhoto
     }
-    
+
+    static func save(_ image: UIImage) -> Observable<String> {
+        //after saving a photo, emit a single element
+        return Observable.create({ observer in
+            var savedAssetId: String?
+
+            PHPhotoLibrary.shared().performChanges({
+                //create a photo assest our of the provided image
+                let request = PHAssetChangeRequest.creationRequestForAsset(from: image)
+                savedAssetId = request.placeholderForCreatedAsset?.localIdentifier
+            }, completionHandler: { success, error in
+                //emit assetId or error
+                DispatchQueue.main.async {
+                    if success, let id = savedAssetId {
+                        observer.onNext(id)
+                        observer.onCompleted()
+                    } else {
+                        observer.onError(error ?? Errors.couldNotSavePhoto)
+                    }
+                }
+            })
+
+            return Disposables.create()
+        })
+    }
     
 }
