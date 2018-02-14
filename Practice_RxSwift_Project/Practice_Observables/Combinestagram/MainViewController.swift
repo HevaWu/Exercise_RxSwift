5806212987ebe52e8e918c1329f708025b68c92f
He Wu <he.wu@linecorp.com>
2018-02-14 15:16:42 +0900

    Format coding

--- Practice_RxSwift_Project/Practice_Observables/Combinestagram/MainViewController.swift
+++ Practice_RxSwift_Project/Practice_Observables/Combinestagram/MainViewController.swift
@@ -25,31 +25,31 @@ import RxSwift
 
 class MainViewController: UIViewController {
 
-  @IBOutlet weak var imagePreview: UIImageView!
-  @IBOutlet weak var buttonClear: UIButton!
-  @IBOutlet weak var buttonSave: UIButton!
-  @IBOutlet weak var itemAdd: UIBarButtonItem!
+    @IBOutlet weak var imagePreview: UIImageView!
+    @IBOutlet weak var buttonClear: UIButton!
+    @IBOutlet weak var buttonSave: UIButton!
+    @IBOutlet weak var itemAdd: UIBarButtonItem!
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-  }
-  
-  @IBAction func actionClear() {
+    }
 
-  }
+    @IBAction func actionClear() {
 
-  @IBAction func actionSave() {
+    }
 
-  }
+    @IBAction func actionSave() {
 
-  @IBAction func actionAdd() {
+    }
 
-  }
+    @IBAction func actionAdd() {
 
-  func showMessage(_ title: String, description: String? = nil) {
-    let alert = UIAlertController(title: title, message: description, preferredStyle: .alert)
-    alert.addAction(UIAlertAction(title: "Close", style: .default, handler: { [weak self] _ in self?.dismiss(animated: true, completion: nil)}))
-    present(alert, animated: true, completion: nil)
-  }
+    }
+
+    func showMessage(_ title: String, description: String? = nil) {
+        let alert = UIAlertController(title: title, message: description, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Close", style: .default, handler: { [weak self] _ in self?.dismiss(animated: true, completion: nil)}))
+        present(alert, animated: true, completion: nil)
+    }
 }
